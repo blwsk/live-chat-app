41a10ce90c907164f2b33cf81ee66c21bdd46599
Kevin Bielawski <kbielawski@hubspot.com>
2018-12-14 14:10:27 -0500

    update useRefreshWidget hook

--- src/hooks/useRefreshWidget.js
+++ src/hooks/useRefreshWidget.js
@@ -2,15 +2,9 @@ import { useEffect } from 'react';
 
 export const useRefreshWidget = (...args) => {
   useEffect(() => {
-    if (
-      window &&
-      window.hubspot &&
-      window.hubspot.messages &&
-      window.hubspot.messages.EXPERIMENTAL_API &&
-      window.hubspot.messages.EXPERIMENTAL_API.requestWidgetRefresh
-    ) {
-      console.log('request widget open');
-      window.hubspot.messages.EXPERIMENTAL_API.requestWidgetRefresh(...args);
+    if (window.HubSpotConversations && window.HubSpotConversations.refresh) {
+      console.log('refreshing widget...');
+      window.HubSpotConversations.refresh(...args);
     } else {
       console.log('this did nothing');
     }
