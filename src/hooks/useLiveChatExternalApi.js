21c2729061084c691b6d6599b9f5568a9ddf438c
Kevin Bielawski <kbielawski@hubspot.com>
2019-01-31 16:04:17 -0500

    use widget.refresh

--- src/hooks/useLiveChatExternalApi.js
+++ src/hooks/useLiveChatExternalApi.js
@@ -2,20 +2,21 @@ import { useEffect } from 'react';
 
 export const useLiveChatExternalApi = () => {
   useEffect(() => {
-    const isApiPresent = window.HubSpotConversations
-      && window.HubSpotConversations.debug
-      && window.HubSpotConversations.refresh;
-    
+    const isApiPresent =
+      window.HubSpotConversations &&
+      window.HubSpotConversations.debug &&
+      window.HubSpotConversations.widget;
+
     if (isApiPresent) {
       console.log('>>> calling HubSpotConversations directly <<<');
       window.HubSpotConversations.debug(true);
-      window.HubSpotConversations.refresh();
+      window.HubSpotConversations.widget.refresh();
     } else {
       window.hsConversationsOnReady = [
         () => {
           console.log('>>> using hsConversationsOnReady callback array <<<');
           window.HubSpotConversations.debug(true);
-          window.HubSpotConversations.refresh();
+          window.HubSpotConversations.widget.refresh();
         }
       ];
     }
